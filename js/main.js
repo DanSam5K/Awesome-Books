9cabd437651c4d6d2c6df45d74554b0eedcd81ef
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-10 03:59:13 +0500

    Create list of books using Javascript

--- js/main.js
+++ js/main.js
@@ -5,19 +5,58 @@ const addBtn = document.querySelector('.add-btn');
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
 const form = document.querySelector('#form');
+const bookLists = document.createElement('div');
+const lists = document.querySelector('#lists');
 
-addBtn.onclick = (e) => {
+// addBtn.onclick = (e) => {
+//   e.preventDefault();
+//   if (title.value === '' && author.value === '') {
+//     alert('Please enter the title and author');
+//     return;
+//   }
+
+//   const option = new Option(title.value, author.value);
+
+//   form.add(option, undefined);
+//   title.value = '';
+//   author.value = '';
+//   title.focus();
+//   author.focus();
+// };
+
+addBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  if(title.value === '' && author.value === '') {
-    alert('Please enter the title and author')
-    return 
+  if (title.value === '' && author.value === '') {
+    alert('Please enter the title and author');
   }
+  const obj = Object.create({});
+  obj.title = title.value;
+  obj.author = author.value;
+  books.push(obj);
+});
 
-  const option = new Option(title.value, author.value) 
-  
-  form.add(option, undefined)
-  title.value = '';
-  author.value = '';
-  title.focus();
-  author.focus();
+console.log('Books Length: ', books.length);
+let length = books.length;
+for (let i = 0; i < length; i++) {
+  const bookTitle = document.createElement('p');
+  bookTitle.innerText = book.title;
+  console.log('Book title: ', books[i].title);
+  const bookAuthor = document.createElement('p');
+  bookAuthor.innerText = books[i].author;
+
+  bookLists.appendChild(bookAuthor);
+  bookLists.appendChild(bookTitle);
 }
+
+books.forEach((book) => {
+  const bookTitle = document.createElement('p');
+  bookTitle.innerText = book.title;
+  console.log('Book title: ', book.title);
+  const bookAuthor = document.createElement('p');
+  bookAuthor.innerText = book.author;
+
+  bookLists.appendChild(bookAuthor);
+  bookLists.appendChild(bookTitle);
+});
+
+lists.append(bookLists);
