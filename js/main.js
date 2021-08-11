57650ce38a7f1d60ddb16d193e568b10d4da244e
Daniel Samuel <dansam5000@gmail.com>
2021-08-11 18:04:35 +0100

    Fix linters errors

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Book Class: Represents a book
+// Book Class: Represents a book
 class Book {
   constructor(title, author) {
     this.title = title;
@@ -6,7 +6,7 @@ class Book {
   }
 }
 
-//UI Class: handle UI tasks
+// UI Class: handle UI tasks
 
 class UI {
   static displayBooks() {
@@ -61,11 +61,13 @@ class Store {
 
     return books;
   }
+
   static addBook(book) {
     const books = Store.getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   }
+
   static removeBooks(author) {
     const books = Store.getBooks();
 
@@ -79,40 +81,40 @@ class Store {
   }
 }
 
-//Events: display books
+// Events: display books
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-//Events: to add a Book
+// Events: to add a Book
 document.querySelector('#book-form').addEventListener('submit', (e) => {
-    // Prevent actual submit
-    e.preventDefault();
-   //get form values
-   const title = document.querySelector('#title').value;
-   const author = document.querySelector('#author').value;
-   //validate 
-   if(title === '' || author === '') {
-       UI.showAlert("Please fill in all fields", "success")
-   } else {
-    //Instantiate book
-   const book = new Book(title, author);
-   //add book to UI
-   UI.addBookToList(book);
-   //addbook to store
-   Store.addBook(book);
-   //Show success
-   UI.showAlert('Book Added', 'success');
-   //clear fields
-   UI.clearFields()
-   }
-})
+  // Prevent actual submit
+  e.preventDefault();
+  // get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  // validate
+  if (title === '' || author === '') {
+    UI.showAlert('Please fill in all fields', 'success');
+  } else {
+    // Instantiate book
+    const book = new Book(title, author);
+    // add book to UI
+    UI.addBookToList(book);
+    // addbook to store
+    Store.addBook(book);
+    // Show success
+    UI.showAlert('Book Added', 'success');
+    // clear fields
+    UI.clearFields();
+  }
+});
 
-//Events: to remove a book
+// Events: to remove a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-    //remove book from UI
-    UI.deleteBook(e.target);
-    //remove book from store
-    Store.removeBooks(e.target.parentElement.previousElementSibling.textContent);
-    //sucess delete
-    UI.showAlert('Book deleted', 'success');
-})
+  // remove book from UI
+  UI.deleteBook(e.target);
+  // remove book from store
+  Store.removeBooks(e.target.parentElement.previousElementSibling.textContent);
+  // sucess delete
+  UI.showAlert('Book deleted', 'success');
+});
