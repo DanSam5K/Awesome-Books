bf48b0a24e7ad09159fcfc88e0ddb33a168ad610
Daniel Samuel <dansam5000@gmail.com>
2021-08-11 16:54:36 +0100

    Create Book class constructor object

--- js/main.js
+++ js/main.js
@@ -1,60 +1,11 @@
-const form = document.querySelector('#form');
-const bookTitleField = document.querySelector('#book-title');
-const bookAuthorField = document.querySelector('#book-author');
-const booksListContainer = document.querySelector('#books-list');
-const template = document.querySelector('#list-item-template');
-const LOCAL_STORAGE_PREFIX = 'AWESOME_BOOKS';
-const BOOKS_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}-books`;
 
-function renderBookInfo(bookInfo) {
-  const templateClone = template.content.cloneNode(true);
-  const listItem = templateClone.querySelector('.list-item');
-  listItem.dataset.booksId = bookInfo.id;
-  const renderTitle = templateClone.querySelector(
-    '[data-list-item-book-title]',
-  );
-  const renderAuthor = templateClone.querySelector(
-    '[data-list-item-book-author]',
-  );
-  renderTitle.innerText = bookInfo.title;
-  renderAuthor.innerText = bookInfo.author;
-  booksListContainer.appendChild(templateClone);
+//Book Class: Represents a book
+class Book {
+    constructor(title, author){
+        this.title = title;
+        this.author = author;
+    }
 }
 
-function loadBookInfo() {
-  const bookString = localStorage.getItem(BOOKS_STORAGE_KEY);
-  return JSON.parse(bookString) || [];
-}
 
-let books = loadBookInfo();
-books.forEach((book) => renderBookInfo(book));
 
-function saveBookInfo() {
-  localStorage.setItem(BOOKS_STORAGE_KEY, JSON.stringify(books));
-}
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const bookTitle = bookTitleField.value;
-  const bookAuthor = bookAuthorField.value;
-  if (bookTitle === '' && bookAuthor === '') return;
-  const bookInfo = {
-    title: bookTitle,
-    author: bookAuthor,
-    id: new Date().valueOf().toString(),
-  };
-  books.push(bookInfo);
-  renderBookInfo(bookInfo);
-  saveBookInfo();
-  bookTitleField.value = '';
-  bookAuthorField.value = '';
-});
-
-booksListContainer.addEventListener('click', (e) => {
-  if (!e.target.matches('[data-button-delete]')) return;
-  const parent = e.target.closest('.list-item');
-  const bookID = parent.dataset.booksId;
-  books = books.filter((book) => book.id !== bookID);
-  parent.remove();
-  saveBookInfo();
-});
